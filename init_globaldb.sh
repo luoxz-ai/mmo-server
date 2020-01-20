534255472cc7598a67476563b1c6446544f02b76
yanwei <yanwei1983@gmail.com>
2020-01-20 11:21:53 +0800

    change dir

--- init_globaldb.sh
+++ init_globaldb.sh
@@ -2,9 +2,9 @@ GLOBAL_IP=172.28.1.3
 GLOBAL_PORTD_START=17000
 
 echo "create database global;" | docker exec -i mysql-global sh -c 'exec mysql -uroot -p"3Killyou"'
-cat server-code/bin/res/db/globaldb.sql | docker exec -i mysql-global sh -c 'exec mysql -uroot -p"3Killyou" global'
+cat server-code/res/db/globaldb.sql | docker exec -i mysql-global sh -c 'exec mysql -uroot -p"3Killyou" global'
 
-cmd="m4 -DGLOBAL_IP=${GLOBAL_IP} -DGLOBAL_PORT_START=${GLOBAL_PORTD_START} /data/mmorpg/server-code/bin/res/db/init_globalservice.sql.template"
+cmd="m4 -DGLOBAL_IP=${GLOBAL_IP} -DGLOBAL_PORT_START=${GLOBAL_PORTD_START} /data/mmorpg/server-code/res/db/init_globalservice.sql.template"
 
 docker exec -i mmo-server-build sh -c "${cmd}" |
 docker exec -i mysql-global sh -c 'exec mysql -uroot -p"3Killyou" global'
--- init_newzonedb.sh
+++ init_newzonedb.sh
@@ -11,13 +11,13 @@ ZONE_OPEN_TIME="\"NOW()\""
 echo "create database ${ZONE_NAME};" | docker exec -i ${MYSQL_DOCKER_NAME} sh -c 'exec mysql -uroot -p"3Killyou"'
 
 sql_cmd="exec mysql -uroot -p'3Killyou' ${ZONE_NAME}"
-cat server-code/bin/res/db/gamedb.sql | docker exec -i ${MYSQL_DOCKER_NAME} sh -c "${sql_cmd}"
+cat server-code/res/db/gamedb.sql | docker exec -i ${MYSQL_DOCKER_NAME} sh -c "${sql_cmd}"
 
 
 
 cmd="m4 -DZONE_ID=${ZONE_ID} -DZONE_NAME=${ZONE_NAME} -DZONE_IP=${ZONE_IP} \
 -DPORT_START=${PORT_START} -DOUT_PORT_START=${OUT_PORT_START} -DZONE_MYSQL_IP=${ZONE_MYSQL_IP} \
--DMYSQL_PASSWD=${MYSQL_PASSWD} -DZONE_OPEN_TIME=${ZONE_OPEN_TIME} /data/mmorpg/server-code/bin/res/db/add_new_server.sql.template"
+-DMYSQL_PASSWD=${MYSQL_PASSWD} -DZONE_OPEN_TIME=${ZONE_OPEN_TIME} /data/mmorpg/server-code/res/db/add_new_server.sql.template"
 
 
 docker exec -i mmo-server-build sh -c "${cmd}" | 
