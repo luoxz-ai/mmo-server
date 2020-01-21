5dfeb0c2624a6a41c6ae82039d769fe40711f7c3
yanwei <yanwei1983@gmail.com>
2020-01-21 16:19:54 +0800

    fix code

--- server-code/src/service/zone_service/item/ItemData.cpp
+++ server-code/src/service/zone_service/item/ItemData.cpp
@@ -52,7 +52,7 @@ bool CItemData::Init(CMysqlConnection* pDB, OBJID idItem)
 	CHECKF(m_pRecord != nullptr);
 	std::string jsonTxt = m_pRecord->Field(TBLD_ITEM::EXTRA);
 	pb_util::LoadFromJsonTxt(jsonTxt, m_ExtraData);
-	m_pRecord->Field(TBLD_ITEM::EXTRA).SetGetValStringFunc( std::bind(&SaveItemExtraToJson, std::const_ref(m_ExtraData)) );
+	m_pRecord->Field(TBLD_ITEM::EXTRA).SetGetValStringFunc( std::bind(&SaveItemExtraToJson, std::cref(m_ExtraData)) );
 
 	m_pType = ItemTypeSet()->QueryObj(GetType());
 	if(!m_pType)
@@ -101,7 +101,7 @@ bool CItemData::Init(CMysqlConnection* pDB, ST_ITEMINFO& info)
 
 	if(m_pRecord->Update() == false)
 		return false;
-	m_pRecord->Field(TBLD_ITEM::EXTRA).SetGetValStringFunc( std::bind(&SaveItemExtraToJson, std::const_ref(m_ExtraData)) );
+	m_pRecord->Field(TBLD_ITEM::EXTRA).SetGetValStringFunc( std::bind(&SaveItemExtraToJson, std::cref(m_ExtraData)) );
 	uint32_t nAddition = GetAddition();
 	if(nAddition > 0)
 	{
@@ -131,7 +131,7 @@ bool CItemData::Init(CMysqlConnection* pDB, uint64_t idPlayer, uint32_t idItemTy
 	m_pRecord->Field(TBLD_ITEM::PILENUM)  = nNum;
 	m_pRecord->Field(TBLD_ITEM::POSITION) = nPosition;
 	m_pRecord->Field(TBLD_ITEM::FLAG)	  = dwFlag;
-	m_pRecord->Field(TBLD_ITEM::EXTRA).SetGetValStringFunc( std::bind(&SaveItemExtraToJson, std::const_ref(m_ExtraData)) );
+	m_pRecord->Field(TBLD_ITEM::EXTRA).SetGetValStringFunc( std::bind(&SaveItemExtraToJson, std::cref(m_ExtraData)) );
 	return true;
 	__LEAVE_FUNCTION
 	return false;
