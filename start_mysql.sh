4f8776aa4ba538167023e70add3fb67277cb9419
yanwei <yanwei1983@gmail.com>
2020-04-17 15:42:42 +0800

    travic fix docker

--- start_mysql.sh
+++ start_mysql.sh
@@ -1,2 +1,2 @@
-docker run -d --restart always --privileged=true --network=network-mmo --ip=172.28.1.2 -e MYSQL_ROOT_PASSWORD=3Killyou -v e:\mmo-server\data\mysql-conf:/etc/mysql/conf.d -v e:\mmo-server\data\mysql-global:/var/lib/mysql --name mysql-global mysql:5.7
-docker run -d --restart always --privileged=true --network=network-mmo --ip=172.28.2.2 -e MYSQL_ROOT_PASSWORD=3Killyou -v e:\mmo-server\data\mysql-conf:/etc/mysql/conf.d -v e:\mmo-server\data\mysql-zone1:/var/lib/mysql --name mysql-zone-1 mysql:5.7
+docker run -d --restart always --privileged=true --network=network-mmo --ip=172.28.1.2 -e MYSQL_ROOT_PASSWORD=3Killyou -v /$(pwd)/data/mysql-conf:/etc/mysql/conf.d -v /$(pwd)/data/mysql-global:/var/lib/mysql --name mysql-global mysql:5.7
+docker run -d --restart always --privileged=true --network=network-mmo --ip=172.28.2.2 -e MYSQL_ROOT_PASSWORD=3Killyou -v /$(pwd)/data/mysql-conf:/etc/mysql/conf.d -v /$(pwd)/data/mysql-zone1:/var/lib/mysql --name mysql-zone-1 mysql:5.7
