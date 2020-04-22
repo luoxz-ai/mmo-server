32e8a2a4d0fecc5d27d053df149a9b078ddacd11
yanwei <yanwei1983@gmail.com>
2020-04-22 17:52:03 +0800

    fix all shell

--- server-depends/src/build_all.sh
+++ server-depends/src/build_all.sh
@@ -3,20 +3,20 @@ cd `dirname $0`
 DIR_file=`pwd`
 
 
-build_depends.sh base;
-build_depends.sh jemalloc;
-build_depends.sh protobuf;
-build_depends.sh cpp_redis;
-build_depends.sh gflags;
-build_depends.sh snappy;
-build_depends.sh leveldb;
-build_depends.sh brpc;
-build_depends.sh libevent;
-build_depends.sh tinyxml2;
-build_depends.sh lua;
-build_depends.sh curlpp;
-build_depends.sh xlnt;
-build_depends.sh fmt;
-build_depends.sh libwebsockets;
-build_depends.sh recastnavigation;
-build_depends.sh box2d;
+bash build_depends.sh base;
+bash build_depends.sh jemalloc;
+bash build_depends.sh protobuf;
+bash build_depends.sh cpp_redis;
+bash build_depends.sh gflags;
+bash build_depends.sh snappy;
+bash build_depends.sh leveldb;
+bash build_depends.sh brpc;
+bash build_depends.sh libevent;
+bash build_depends.sh tinyxml2;
+bash build_depends.sh lua;
+bash build_depends.sh curlpp;
+bash build_depends.sh xlnt;
+bash build_depends.sh fmt;
+bash build_depends.sh libwebsockets;
+bash build_depends.sh recastnavigation;
+bash build_depends.sh box2d;
