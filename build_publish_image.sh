9a2d62e51be5a3dcb9265fccf41d517f05eeabd8
yanwei <yanwei1983@gmail.com>
2020-04-28 10:07:28 +0800

    github action

--- build_publish_image.sh
+++ build_publish_image.sh
@@ -7,7 +7,7 @@ cmd="./releasebuild.sh && cd bin && ./cp_depends_lib.sh && ./cp_release.sh"
 
 ln -sf .dockerignore.publish .dockerignore
 
-docker run --rm --name mmo-server-publish --volumes-from mmo-server-code --privileged --ulimit core=-1 --security-opt seccomp=unconfined mmo-server-depends sh -c "${cmd}"
+docker run --rm --name mmo-server-publish --privileged --ulimit core=-1 --security-opt seccomp=unconfined -v /$(pwd)/server-code:/data/mmorpg/server-code -v /$(pwd)/server-res:/data/mmorpg/server-res -v /$(pwd)/share-code:/data/mmorpg/share-code --user=ubuntu mmo-server-depends sh -c "${cmd}"
 
 docker build --rm -f "server-docker/mmo-server-publish.Dockerfile" -t mmo-server:$1 .
 docker tag mmo-server:$1 mmo-server:latest
