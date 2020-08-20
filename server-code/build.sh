75f34411c6077594d98386988337b1bb35310353
yanwei <yanwei1983@gmail.com>
2020-08-20 17:02:40 +0800

    enable CCACHE_SLOPPINESS

--- server-code/build.sh
+++ server-code/build.sh
@@ -5,6 +5,6 @@ DIR_file=`pwd`
 mkdir -p build
 cd build/
 set -e
-CC=clang-9 CXX=clang++-9 cmake .. -DDEBUG=ON -DUSE_ASAN=ON -DUSE_UBSAN=ON
+CCACHE_SLOPPINESS=pch_defines,time_macros CC=clang-9 CXX=clang++-9 cmake .. -DDEBUG=ON -DUSE_ASAN=ON -DUSE_UBSAN=ON
 #CC=gcc-7 CXX=g++-7 cmake .. -DDEBUG=ON
 make -j3
--- server-code/releasebuild.sh
+++ server-code/releasebuild.sh
@@ -5,5 +5,5 @@ DIR_file=`pwd`
 mkdir -p build
 cd build/
 set -e
-CC=clang-9 CXX=clang++-9 cmake .. -DDEBUG=OFF -DTIDY=OFF
+CCACHE_SLOPPINESS=pch_defines,time_macros CC=clang-9 CXX=clang++-9 cmake .. -DDEBUG=OFF -DTIDY=OFF
 make -j4
